f5543b7fb12d587647af56818ab9d2b70584ea40
Warley <warleyyb17@gmail.com>
2025-01-27 16:38:52 -0300

    Add files via upload

--- ship.py
+++ ship.py
@@ -9,7 +9,7 @@ class Ship(Sprite):
         self.screen = screen
         self.ai_settings = ai_settings
 
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
